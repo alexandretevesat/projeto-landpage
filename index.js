f178772bd4d6a01c4b3e03836bfb63de2095396e
unknown <alexandreteves0834@digitalcollege.internal>
2025-10-22 15:47:52 -0300

    Desenvolvido até as 15:50

---
+++ index.js
@@ -0,0 +1,7 @@
+document.addEventListener("scroll", () => {
+  if (window.pageYOffset > 0) {
+    document.querySelector("header").classList.add("active");
+  } else {
+    document.querySelector("header").classList.remove("active");
+  }
+});
